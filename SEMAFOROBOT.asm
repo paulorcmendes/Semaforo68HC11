3031c143292900ebf57b4fcbdb99d2d987f6e75c
feliphechaves <40308692+feliphechaves@users.noreply.github.com>
2018-06-15 23:32:46 -0300

    Update SEMAFOROBOT.asm

--- SEMAFOROBOT.asm
+++ SEMAFOROBOT.asm
@@ -7,6 +7,8 @@
 SEMG	EQU	$0000
 SEMY	EQU	$0001
 SEMR	EQU	$0002
+SEMRP	EQU	$0003
+QTDP	EQU	$0004
 PORTB	EQU	$1004
 PORTC	EQU	$1003
 DDRC	EQU	$1007
@@ -14,6 +16,8 @@ DDRC	EQU	$1007
 	FCB	%00000110
 	FCB	%00001010
 	FCB	%00010001
+	FCB	%00010000
+	FCB	5
 	FCB	$01
 *inutilizando outros pinos de c
 	ldaa	#$00
@@ -38,6 +42,7 @@ FECHA	JSR	DELAYB
 	LDAA	SEMR
 	STAA	PORTB
 	JSR	DELAYRP
+	JSR	PISCAR
 	rts
 
 *reiniciar PC0
@@ -49,12 +54,10 @@ RESTART	ldaa	#$00
 	staa	ddrc
 	rts
 
-
 delayYP	ldy	#10000
 cont1	dey
 	bne	cont1
 	rts
-
 
 delayRP	ldy	#20000
 cont2	dey
@@ -64,4 +67,25 @@ cont2	dey
 delayB	ldy	#10000
 cont3	dey
 	bne	cont3
+	rts
+
+*piscar 
+piscar	ldab	QTDP	
+	ldaa	SEMRP
+	staa	PORTB
+	jsr	delay1
+	ldaa	SEMR
+	staa	PORTB
+	jsr	delay1
+	decb	
+	stab	QTDP
+	bne	piscar
+	ldab	#5
+	stab	QTDP
+	rts	
+
+
+delay1	ldy	#500
+cont4	dey
+	bne	cont4
 	rts
